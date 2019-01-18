cf8c254e03a220cfeaddfd946c1d2797f95036ab
PauLiLin <lilin.pau@gmail.com>
2019-01-18 23:23:10 +0100

    remove residual lines

--- src/main.py
+++ src/main.py
@@ -8,13 +8,9 @@ if __name__ == "__main__":
     # City
     city = City(rows=20, cols=20, n_intersections=4)
     city.grid.generate(seed=12)
-  
-    start_p = city.grid.get(1,4)
-    final_p = city.grid.get(1,11)
     
     destinations = generate_bfs_matrix(city.grid)
-   
-    
+       
     drawer = Drawer(fps_target=30, city=city, width=800, height=800, margin=0)
     drawer.run()
     #t = threading.Thread(target=drawer.run, args=[])
