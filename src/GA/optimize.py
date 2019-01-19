7dce7cb5703b99d1d2a5d13147f1e79fefbd52e8
Raquel <raquelpa93@gmail.com>
2019-01-19 23:26:20 +0100

    fixe merge

---
+++ src/GA/optimize.py
@@ -0,0 +1,62 @@
+import numpy as np
+from GA.Population import Population
+from multiprocessing import Pool
+from city import City
+from time import time
+from datetime import timedelta
+
+PROCESSES = 6
+ENABLE_MULTIPROCESSING = True
+
+def generate_args(cities, number_of_lights, population, n_simulations, steps_simulation):
+    args = []
+    for i, g in enumerate(population.genes):
+        cities[i].clean()
+        args.append([cities[i], g.gene, number_of_lights, n_simulations, steps_simulation])
+    return args
+
+def run_genetics(rows, cols, n_intersections, seed, max_generations=40, max_sim_steps=200, num_sim=5):
+    dummy = City(rows, cols, n_intersections, seed)
+    number_of_lights = len(dummy.grid.roads_with_lights)
+
+    population = Population(generation_id=0, pop_size=20, dna_size=max_sim_steps * number_of_lights, elitism_n=100,
+                            truncation_percentage=0.33, cross_over_points=50,
+                            crossover_probability=0.9, mutation_probability=0.005, multiprocessing=False)
+
+    print('Maximum population size: ', population.max_pop_size())
+    cities = [City(rows, cols, n_intersections, seed) for _ in range(population.max_pop_size())]
+
+    best_gene = []
+    best_performance = 0
+    for generation in range(max_generations):
+        init = time()
+        print('Step:', generation, end=' ')
+        args = generate_args(cities, number_of_lights, population, num_sim, max_sim_steps)
+        if ENABLE_MULTIPROCESSING:
+            pool = Pool(PROCESSES)
+            scores = pool.starmap(run_gene, args)
+            pool.close()
+            for i, s in enumerate(scores):
+                population.genes[i].score = s
+        else:
+            for gene in population.genes:
+                gene.score = run_gene(gene)
+
+        best_performance, best_gene, pop_size = population.update_genes()
+        print('| New pop size:', pop_size, '| Best fitness:', best_performance, '| Took', timedelta(seconds=(time() - init)), '| Best gene:', best_gene)
+    return best_performance, best_gene
+
+
+# city, gene, number_of_lights, n_simulations, steps_simulation
+def run_gene(city, gene, number_of_lights, n_simulations, steps_simulation):
+    average_fitness = []
+    for single_simulation in range(n_simulations):
+        lights_gene = gene
+        lights_gene = np.reshape(lights_gene, [number_of_lights, steps_simulation]).T
+        for i in range(steps_simulation):
+            lights = lights_gene[i, :]
+            city.step(lights)
+        fitness = city.cars_despawned
+        average_fitness.append(fitness)
+        city.clean()
+    return np.mean(average_fitness)
---
+++ src/pathing.py
@@ -0,0 +1,93 @@
+from collections import defaultdict
+from queue import Queue
+
+
+def set_shortest_paths(roads):
+    in_borders = [road for road in roads if len(road.parents) == 0]
+    out_borders = [road for road in roads if len(road.children) == 0]
+    destinations = defaultdict(dict)
+    dependencies = defaultdict(list)
+    parent = defaultdict(lambda: None)
+    visited = defaultdict(lambda: False)
+
+    # Fill unreachable
+    for origin in out_borders:
+        for destination in roads:
+            destinations[origin][destination] = None
+
+    for origin in roads:
+        for destination in in_borders:
+            destinations[origin][destination] = None
+
+    # Fill diagonal
+    for road in roads:
+        destinations[road][road] = road
+
+    # Explore tree
+    for border in in_borders:
+        queue = Queue()
+        queue.put(border)
+        while not queue.empty():
+            curr = queue.get()
+            for child in curr.children:
+                if not visited[child]:
+                    parent[child] = curr
+                    visited[child] = True
+                    queue.put(child)
+                    __save_path(parent, child, destinations)
+                else:
+                    dependencies[child].append(curr)
+                    __save_path_dependency(parent, curr, child, destinations)
+
+    __copycat(parent, dependencies, destinations)
+    for origin in roads:
+        origin.destinations = dict()
+        for destination in roads:
+            if destination not in destinations[origin]:
+                origin.destinations[destination] = None
+            else:
+                origin.destinations[destination] = destinations[origin][destination]
+
+    # for road in roads:
+        #     road.destinations = defaultdict(lambda: None)
+            # for k, v in destinations[road].items():
+    # road.destinations[k] = v
+
+
+def __copycat(parent, dependencies, destinations):
+    for key in dependencies.keys():
+        for dependency in dependencies[key]:
+            __resolve(parent, dependency, key, destinations)
+
+
+def __resolve(parent, origin, child, destinations):
+    curr = origin
+    last = child
+
+    while curr is not None:
+        for destination in destinations[child]:
+            if destination not in destinations[curr]:
+                destinations[curr][destination] = last
+        last = curr
+        curr = parent[curr]
+
+
+def __save_path(parent, child, destinations):
+    last = child
+    curr = child
+
+    while parent[curr] is not None:
+        curr = parent[curr]
+        destinations[curr][child] = last
+        last = curr
+
+
+def __save_path_dependency(parent, other_parent, child, destinations):
+    last = child
+    curr = other_parent
+
+    destinations[curr][child] = last
+    while parent[curr] is not None:
+        curr = parent[curr]
+        destinations[curr][child] = last
+        last = curr
